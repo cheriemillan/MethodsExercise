dba4508feb382e652dc503d9163ca906be7c80de
CherieMillan <cherie99millan@gmail.com>
2024-07-10 10:32:06 -0400

    Put in a method instead of putting it in main

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -2,7 +2,7 @@
 {
     public class Program
     {
-        static void Main(string[] args)
+        public static void Questions()
         {
             Console.WriteLine("What is your name?");
             string name= Console.ReadLine();
@@ -17,6 +17,10 @@
             
             Console.WriteLine("Let's write a fun short story with the answers you provided!\n");
             Console.WriteLine($"Hello my name is {name}! A long time ago I saw {favBand} in person. They are my favorite band. I decided to wear {favColor} because it's also my favorite color. Then on the way home I saw a {favAnimal} my favorite animal! What a crazy lucky day right?");
+        }
+        static void Main(string[] args)
+        {
+          Questions();
         }
     }
 }
